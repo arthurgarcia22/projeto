049730ee9df1194817562823543460ed8d624abe
arthurgarcia22 <161314005+arthurgarcia22@users.noreply.github.com>
2024-10-08 12:12:00 -0300

    a

---
+++ perguntas.js
@@ -0,0 +1,125 @@
+criaCartao(
+    'Matemática'
+    'Como se calcula a subtração de dois termmos?'
+    'Utiliza-se a fórmula: (a1-a2)'
+    )
+
+criaCartao(
+    Matemática' 
+    'Como se calcula a soma de dois números?' 
+    'Utiliza-se a fórmula: (a1 + a2)' 
+    )
+
+    criaCartao(
+    'Física' 
+    'O que é velocidade média?' 
+    'A fórmula é: Vm = ΔS/ΔT (variação de espaço dividido pela variação de tempo)'
+    )
+  
+    criaCartao(
+    'História' 
+    'Quem foi o responsável pelo Descobrimento do Brasil?'
+    'Pedro Álvares Cabral, em 1500’ 
+    )
+
+    criaCartao(
+    'Química' 
+    'O que é uma reação de neutralização?' 
+    'É a reação entre um ácido e uma base que produz sal e água.'
+    )
+
+    criaCartao(
+    'Geografia'
+    'O que é latitude?'
+    'É a distância em graus de um ponto na Terra em relação à Linha do Equador.'
+    )
+  
+    criaCartao(
+    'Literatura' 
+    'Quem escreveu Dom Casmurro?'
+    'Machado de Assis'
+    )
+ 
+    criaCartao(
+    'Biologia'
+    'O que são células procariontes?'
+    'São células que não possuem núcleo delimitado por membrana.'
+    )
+
+    criaCartao(
+    'Matemática'
+    'Como se calcula a área de um círculo'
+    'Utiliza-se a fórmula: A = πr²'
+    )
+
+    criaCartao(
+    'Filosofia'
+    'Quem é considerado o pai da filosofia?'
+    'Sócrates, na Grécia Antiga.'
+    )
+
+    criaCartao(
+    'Sociologia'
+    'O que é desigualdade social?'
+    'É a disparidade no acesso a recursos, poder e oportunidades dentro de uma sociedade.'
+    )
+    
+    criaCartao(
+    'Matemática' 
+    'Como se calcula o produto de dois números?' 
+    'Multiplica-se a1 por a2: (a1 * a2)' 
+    )
+    
+    criaCartao(
+    'Física' 
+    'O que é aceleração?' 
+    'Aceleração é a variação da velocidade em relação ao tempo: a = Δv/Δt.' 
+    )
+
+criaCartao(
+'Geografia' 
+'O que é longitude?' 
+'É a distância em graus de um ponto na Terra em relação ao Meridiano de Greenwich.' 
+)
+
+criaCartao(
+'Química'
+'O que é um átomo?'
+'É a menor unidade de matéria que conserva as propriedades de um elemento químico.' 
+)
+
+criaCartao(
+'História'
+'O que foi a Revolução Francesa?'
+'Foi um movimento político e social que ocorreu na França entre 1789 e 1799, marcando o fim do absolutismo.' 
+)
+
+criaCartao(
+'Matemática'
+'Como se calcula o quociente de dois números?' 
+'Utiliza-se a fórmula: (a1 ÷ a2)' 
+)
+
+criaCartao(
+'Física' 
+'O que é energia cinética?'
+'Energia cinética é a energia associada ao movimento de um corpo: Ec = (m*v²)/2.'
+)
+
+criaCartao(
+'Biologia'
+'O que é mitose?'
+'Mitose é o processo de divisão celular que resulta em duas células-filhas geneticamente idênticas.'
+)
+
+criaCartao(
+'Geografia'
+'O que é o efeito estufa?'
+'É o processo de aquecimento natural da Terra devido à retenção de calor pelos gases na atmosfera.'
+)
+
+criaCartao(
+'Literatura'
+'Quem escreveu a obra “Os Sertões”?'
+'Euclides da Cunha'
+)
