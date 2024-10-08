08f2dde6a0899639bacbff79dec013356ffa7c83
arthurgarcia22 <161314005+arthurgarcia22@users.noreply.github.com>
2024-10-08 12:19:16 -0300

    a

--- perguntas.js
+++ perguntas.js
@@ -76,50 +76,50 @@ criaCartao(
     'Aceleração é a variação da velocidade em relação ao tempo: a = Δv/Δt.' 
     )
 
-criaCartao(
-'Geografia' 
-'O que é longitude?' 
-'É a distância em graus de um ponto na Terra em relação ao Meridiano de Greenwich.' 
-)
+    criaCartao(
+    'Geografia' 
+    'O que é longitude?' 
+    'É a distância em graus de um ponto na Terra em relação ao Meridiano de Greenwich.' 
+    )
 
-criaCartao(
-'Química'
-'O que é um átomo?'
-'É a menor unidade de matéria que conserva as propriedades de um elemento químico.' 
-)
+    criaCartao(
+    'Química'
+    'O que é um átomo?'
+    'É a menor unidade de matéria que conserva as propriedades de um elemento químico.' 
+    )
 
-criaCartao(
-'História'
-'O que foi a Revolução Francesa?'
-'Foi um movimento político e social que ocorreu na França entre 1789 e 1799, marcando o fim do absolutismo.' 
-)
+    criaCartao(
+    'História'
+    'O que foi a Revolução Francesa?'
+    'Foi um movimento político e social que ocorreu na França entre 1789 e 1799, marcando o fim do absolutismo.' 
+    )
 
-criaCartao(
-'Matemática'
-'Como se calcula o quociente de dois números?' 
-'Utiliza-se a fórmula: (a1 ÷ a2)' 
-)
+    criaCartao(
+    'Matemática'
+    'Como se calcula o quociente de dois números?' 
+    'Utiliza-se a fórmula: (a1 ÷ a2)' 
+    )
 
-criaCartao(
-'Física' 
-'O que é energia cinética?'
-'Energia cinética é a energia associada ao movimento de um corpo: Ec = (m*v²)/2.'
-)
+    criaCartao(
+    'Física' 
+    'O que é energia cinética?'
+    'Energia cinética é a energia associada ao movimento de um corpo: Ec = (m*v²)/2.'
+    )
 
-criaCartao(
-'Biologia'
-'O que é mitose?'
-'Mitose é o processo de divisão celular que resulta em duas células-filhas geneticamente idênticas.'
-)
+    criaCartao(
+    'Biologia'
+    'O que é mitose?'
+    'Mitose é o processo de divisão celular que resulta em duas células-filhas geneticamente idênticas.'
+    )
 
-criaCartao(
-'Geografia'
-'O que é o efeito estufa?'
-'É o processo de aquecimento natural da Terra devido à retenção de calor pelos gases na atmosfera.'
-)
+    criaCartao(
+    'Geografia'
+    'O que é o efeito estufa?'
+    'É o processo de aquecimento natural da Terra devido à retenção de calor pelos gases na atmosfera.'
+    )
 
-criaCartao(
-'Literatura'
-'Quem escreveu a obra “Os Sertões”?'
-'Euclides da Cunha'
-)
+    criaCartao(
+    'Literatura'
+    'Quem escreveu a obra “Os Sertões”?'
+    'Euclides da Cunha'
+    )
